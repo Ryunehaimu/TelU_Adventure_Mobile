ceb9ded480da9bcc24ef4d70960c52f4ee28b811
Ryunehaimu <ryunehaimu007@gmail.com>
2024-03-19 20:18:04 +0700

    start,login,register,landing\

---
+++ telu_adventure/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'page/landing_page.dart';
+import 'page/start_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const landing_page()),
+      );
+    });
+    return const start();
+  }
+}
---
+++ telu_adventure/lib/page/register_page.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:telu_adventure/page/login_page.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            left: 30,
+            top: 50,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/img/logo_datar.png',
+                  height: 50,
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 50),
+                  child: Column(
+                    // Wrap both Text widgets in a Column to display them vertically
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Daftar',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Inter',
+                        ),
+                      ),
+                      Text(
+                        'Gabung ke TelU Adventure',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Inter',
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0),
+                        child: Text(
+                          'Nama Pengguna',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        height: 150,
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Enter your text',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 0.0),
+                        child: Container(
+                          child: Text(
+                            'Password',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'Inter',
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        height: 150,
+                        padding: EdgeInsets.only(top: 16.0),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Enter your password',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xFFCA292E)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          fixedSize:
+                              MaterialStateProperty.all<Size>(Size(300, 50)),
+                        ),
+                        child: Text(
+                          'Daftar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, left: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                // Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                              },
+                              child: Text(
+                                'Sudah punya akun?',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: 'Inter',
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                // Handle navigation to register page
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LoginPage()));
+                              },
+                              child: Text(
+                                ' Masuk sekarang',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: 'Inter',
+                                  color: Colors.red[800], // Merah
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
